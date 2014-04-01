88a5f3c00f92d3363c9ab53511f9d39e07c26666
mattn <mattn.jp@gmail.com>
2014-04-01 18:36:10 +0900

    godoc

--- isatty_linux.go
+++ isatty_linux.go
@@ -9,6 +9,7 @@ import (
 
 const ioctlReadTermios = syscall.TCGETS
 
+// IsTerminal return true if the file descriptor is terminal.
 func IsTerminal(fd uintptr) bool {
 	var termios syscall.Termios
 	_, _, err := syscall.Syscall6(syscall.SYS_IOCTL, fd, ioctlReadTermios, uintptr(unsafe.Pointer(&termios)), 0, 0, 0)
