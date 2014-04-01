02fb6bf60dfa68e12fae2873b5a9700295d925a3
mattn <mattn.jp@gmail.com>
2014-04-01 11:16:42 +0900

    Suppress int()

--- isatty_darwin.go
+++ isatty_darwin.go
@@ -9,8 +9,8 @@ import (
 
 const ioctlReadTermios = syscall.TIOCGETA
 
-func IsTerminal(fd int) bool {
+func IsTerminal(fd uintptr) bool {
 	var termios syscall.Termios
-	_, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(fd), ioctlReadTermios, uintptr(unsafe.Pointer(&termios)), 0, 0, 0)
+	_, _, err := syscall.Syscall6(syscall.SYS_IOCTL, fd, ioctlReadTermios, uintptr(unsafe.Pointer(&termios)), 0, 0, 0)
 	return err == 0
 }
--- isatty_windows.go
+++ isatty_windows.go
@@ -10,8 +10,8 @@ import (
 var kernel32 = syscall.NewLazyDLL("kernel32.dll")
 var procGetConsoleMode = kernel32.NewProc("GetConsoleMode")
 
-func IsTerminal(fd int) bool {
+func IsTerminal(fd uintptr) bool {
 	var st uint32
-	r, _, e := syscall.Syscall(procGetConsoleMode.Addr(), 2, uintptr(fd), uintptr(unsafe.Pointer(&st)), 0)
+	r, _, e := syscall.Syscall(procGetConsoleMode.Addr(), 2, fd, uintptr(unsafe.Pointer(&st)), 0)
 	return r != 0 && e == 0
 }
